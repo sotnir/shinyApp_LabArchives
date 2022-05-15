54b75dda285f8939cee7902da827e6dd7f0d7a07
YukaiZou <yukaizou2012@gmail.com>
2022-05-15 21:12:58 +0100

    Add codes

---
+++ shinyApp_LabArchives.R
@@ -0,0 +1,174 @@
+# Set working directory
+
+# Load packages
+library(shiny); library(shinythemes); library(markdown); library(plyr); library(tidyverse); library(reshape2); library(RColorBrewer); library(lubridate); library(RColorBrewer)
+
+# Define UI ----
+if (interactive()) {
+        ui <- fluidPage(theme = shinytheme("united"),
+                        titlePanel("UoS-UHS Research MRI Booking System"),
+                        
+navbarPage("Menu", id = "myNavbarPage",
+           tabPanel("Overview", 
+                    includeMarkdown("Intro.md")
+                    ),
+           
+           tabPanel("Report", 
+                    sidebarPanel(fileInput("file", label = h3("File input")),
+                                 
+                                 dateInput("date1",
+                                           h3("Start Date:"),
+                                           value = "2022-01-01"
+                                           # value = ifelse(exists("input$file"), yes=min(read.csv(input$file$datapath)$Date), no="2022-01-01")
+                                           ),
+                                 
+                                 dateInput("date2",
+                                           h3("End Date:"),
+                                           value = "2022-02-28"),
+                                 
+                                 selectInput("select1", 
+                                        h3("Select Type:"),
+                                        choices = list("Utility by Type"  = 1, 
+                                        "Monthly Utility" = 2),
+                                        selected = 1),
+                                 
+                                 # Button: Plot
+                                 actionButton("updateButton", "Plot"),
+                                 p("Click the button to create the plot to be displayed in the main panel. If the plot is not updated automatically, please click on this button again.")
+                                 ),
+                                 
+                                 
+                                 mainPanel(textOutput("file_selected"),
+                                          tags$head(tags$style("#file_selected{
+                                                        font-size: 20px;
+                                                        font-style: bold;
+                                                                }")
+                                           ),
+                                           fluidRow(column(width=12, 
+                                             dataTableOutput("file_value"), 
+                                             style = "height:50vh; overflow-y: scroll;overflow-x: scroll;")),
+                                           textOutput("plot_text1"),
+                                           tags$head(tags$style("#plot_text1{
+                                                        font-size: 20px;
+                                                        font-style: bold;
+                                                                }")
+                                           ),
+                                          textOutput("plot_text2"),
+                                           uiOutput("report_ggplot"))
+                                 )
+                    )
+           )
+
+############################# Define server logic #############################
+server <- function(input, output, session) {
+        
+        # Load Data
+        X2Summary_out <- reactive({
+                X2_out <- read.csv(input$file$datapath)
+                return(X2_out)
+        })
+        
+        
+        
+        observeEvent(input$updateButton, {
+                
+                output$file_selected <- renderText({
+                        "File selected:"
+                })
+                
+                output$plot_text1 <- renderText({
+                        "Plot:"
+                })
+                
+                date_start <- as.Date(input$date1)
+                date_end <- as.Date(input$date2)
+                dat_TypeHours <- X2Summary_out() %>% filter(Type != "") %>% 
+                        dplyr::select(Type, Date = Begin, Hours) %>%
+                        transform(Date = as.Date(Date, format = "%d/%m/%Y")) %>%
+                        
+                        # Filter dates based on input
+                        filter(Date > date_start & Date < date_end)
+                
+                date_first <- min(dat_TypeHours$Date)
+                date_last <- max(dat_TypeHours$Date)
+                nWeeks <- difftime(date_last, date_first, units="weeks") %>% 
+                        as.numeric() %>% floor()
+                time_unused <- 15 * nWeeks
+                
+                output$plot_text2 <- renderText({
+                        paste0("First Date: ", date_first,
+                              "; Last Date: ", date_last,
+                              "; Number of Weeks: ", nWeeks,
+                              "; Total Research-protected Time: ", 
+                              time_unused, " hours.")
+                })
+                
+                output$file_value <- renderDataTable({
+                        X2Summary_out()
+                })
+                
+                output$report_ggplot <- renderUI({
+                        renderPlot(expr = if (input$select1 == 1) {
+                                dat_ggplot() %>% ggplot(aes(x = Type , y = Hours)) + 
+                                        geom_col(aes(fill=Type), col="black") +
+                                        scale_fill_brewer(palette="Set1") +
+                                        geom_text(aes(label = Hours), vjust = -0.5) +
+                                        ylab("Hours") + 
+                                        guides(fill=F)
+                        }
+                        else if (input$select1 == 2) {
+                                dat_ggplot() %>% filter(grepl("University Research|UHS Research", Type)) %>%
+                                        # ggplot(aes(x = Month, y = Hours_pct_month)) +
+                                        ggplot(aes(x = Month, y = Hours)) +
+                                        geom_col(aes(fill=Type), col="black") +
+                                        scale_fill_brewer(palette="Set1") +
+                                        geom_text(aes(label = Hours), vjust = -0.5) +
+                                        theme(text = element_text(size = 20)) +
+                                        scale_x_date(date_breaks = "1 month", date_labels="%b-%Y") +
+                                        facet_wrap(Type ~ .) +
+                                        ylab("Hours") +
+                                        guides(fill=F)
+                        })
+                })
+                
+############ Prepare data for plotting ############
+                dat_ggplot <- function() {
+                        # https://stackoverflow.com/questions/6052631/aggregate-daily-data-to-month-year-intervals/6082768#6082768
+                        df_TypeHours <- ddply(dat_TypeHours, "Type", summarise, Hours=sum(Hours))
+                        # Calculate Unused Time: 
+                        # e.g. 15 hours/week * 30 weeks  = 450 hours
+                        df_TypeHours <- df_TypeHours %>%
+                                add_row(Type = "Unused", Hours = time_unused - sum(df_TypeHours$Hours)) %>% 
+                                transform(Hours_percent = round(Hours / time_unused * 100)) %>% 
+                                arrange(desc(Hours))
+                        df_TypeHours$Type <- factor(df_TypeHours$Type, 
+                                            levels = c("University Research",
+                                               "UHS Research & Development",
+                                               "UHS Clinical", 
+                                                "Radiographer Not Available",
+                                               "Scanner Downtime", 
+                                               "Unused"))
+                        
+                        if (input$select1 == 1) {
+                                df_2 <- df_TypeHours
+                        } else if (input$select1 == 2) {
+                                dat_TypeHours$Month <- floor_date(dat_TypeHours$Date, "month")
+                                
+                        df_2 <- ddply(dat_TypeHours, c("Month", "Type"), 
+                        summarise, Hours = tapply(Hours, Type, sum)) %>% 
+                                
+                        # Calculate Monthly Hours: 15 hours/week * 4 weeks/mo = 60 hours/mo
+                        transform(Hours_pct_month = round(Hours / 60 * 100))
+                        df_2$Type <- factor(df_2$Type) %>% 
+                                relevel("University Research")
+                        }
+                        
+                        return(df_2)
+                }
+#################################################
+        })
+}
+
+# Run the app ----
+shinyApp(ui = ui, server = server)
+}
